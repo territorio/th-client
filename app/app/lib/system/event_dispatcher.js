c0b8365bad7cc81089b45a89a96d5581f7dacb27
Pepe Cano <ppcanodehuelva@gmail.com>
2014-06-24 08:40:15 +0200

    Clean code

--- app/app/lib/system/event_dispatcher.js
+++ app/app/lib/system/event_dispatcher.js
@@ -1,12 +1,8 @@
+
 Ember.EventDispatcher.reopen({
   events: {
-    //tap  : 'tap',
-    //click       : 'click', // manage to preventDefault link-to views
-    focusin     : 'focusIn',
     focusout    : 'focusOut',
     keyup       : 'keyUp',
-    submit      : 'submit',
-    input       : 'input',
     change      : 'change'
   },
  
--- app/app/lib/views/landing.js
+++ app/app/lib/views/landing.js
@@ -1,25 +1,3 @@
-/*
-App.landingGestureDelegate = Em.GestureDelegate.create({
-  name: 'landing_gesture_delegate',
-
-  shouldReceiveTouch: function(gesture, view, event) {
-
-    var result = true;
-
-		if ( view instanceof Th.LandingAsideFrontView ) {
-      result = App.applicationController.get('isMenuCategory');
-		} else if ( view instanceof Th.CategoryView ) {
-			return true;
-    } else if ( App.applicationController.get('isMenuCategory') ) {
-      result = false;
-    }
-
-    return result; 
-  }
-
-});
-*/
-
 
 App.LandingScreenView = Th.LandingScreenView.extend(Th.Context, {
 
